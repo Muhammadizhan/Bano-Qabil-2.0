13739e9b3c91c5b0d742ec90f5ce6de52f1cbe90
Muhammadizhan <izhanayaz4@gmail.com>
2024-01-28 16:42:14 +0500

    first commit

---
+++ 2.js
@@ -0,0 +1,15 @@
+// Assignment 2
+let length = 5; 
+let width = 8; 
+
+let area = length * width;
+console.log("Area of the rectangle:", area);
+
+
+let userName = prompt("Please enter your name:");
+if (userName) {
+  
+  alert("Hello, " + userName + "! Welcome to our website.");
+} else {
+  alert("Hello there! Welcome to our website.");
+}
---
+++ js1.js
@@ -0,0 +1,8 @@
+// Assignment 1
+let age = 25; 
+let isStudent = true; 
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+
+alert("Welcome! \nGreetings! I am excited to have you here.");
